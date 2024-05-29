10eb3ea1258cb66c17ce7a69f69d158a25edba54
markusKKK <159111244+markusKKK@users.noreply.github.com>
2024-05-29 17:26:47 +0300

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule} from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatToolbarModule,
     MatIconModule,
-    MatSidenavModule
+    MatSidenavModule,
+    HttpClientModule,
+    MatListModule
   ],
   providers: [
     provideAnimationsAsync()
--- src/app/creditcards/creditcards.component.ts
+++ src/app/creditcards/creditcards.component.ts
@@ -1,173 +1,39 @@
-import { Component, ViewChild, viewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CreditCard } from '../models/credit-card';
-import { MatTable, MatTableDataSource } from '@angular/material/table';
+import { MatTableDataSource } from '@angular/material/table';
 import { SelectionModel } from '@angular/cdk/collections';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
+import { CreditcardsService } from '../services/creditcards.service';
 
-const TABLE_DATA: CreditCard[] = [
-  {
-    id: 1,
-    name: 'Bank of America',
-    description: "Bank of America offers customers with various options",
-    bankName: "Bank of America",
-    maxCredit: 3000,
-    interestRate: 10,
-    active: true,
-    recommendedScore: '700-900',
-    annualFee: 4,
-    termsAndConditions: 'Following are the terms and conditions',
-    createdDate: '2023-31-08',
-    updatedDate: '2023-31-08',
-  },
-  {
-    id: 2,
-    name: 'Bank of America',
-    description: "Bank of America offers customers with various options",
-    bankName: "Bank of America",
-    maxCredit: 3000,
-    interestRate: 10,
-    active: true,
-    recommendedScore: '700-900',
-    annualFee: 4,
-    termsAndConditions: 'Following are the terms and conditions',
-    createdDate: '2023-31-08',
-    updatedDate: '2023-31-08',
-  },
-  {
-    id: 3,
-    name: 'Bank of America',
-    description: "Bank of America offers customers with various options",
-    bankName: "Bank of America",
-    maxCredit: 3000,
-    interestRate: 10,
-    active: true,
-    recommendedScore: '700-900',
-    annualFee: 4,
-    termsAndConditions: 'Following are the terms and conditions',
-    createdDate: '2023-31-08',
-    updatedDate: '2023-31-08',
-  },
-  {
-    id: 3,
-    name: 'Bank of America',
-    description: "Bank of America offers customers with various options",
-    bankName: "Bank of America",
-    maxCredit: 3000,
-    interestRate: 10,
-    active: true,
-    recommendedScore: '700-900',
-    annualFee: 4,
-    termsAndConditions: 'Following are the terms and conditions',
-    createdDate: '2023-31-08',
-    updatedDate: '2023-31-08',
-  },
-  {
-    id: 3,
-    name: 'Bank of America',
-    description: "Bank of America offers customers with various options",
-    bankName: "Bank of America",
-    maxCredit: 3000,
-    interestRate: 10,
-    active: true,
-    recommendedScore: '700-900',
-    annualFee: 4,
-    termsAndConditions: 'Following are the terms and conditions',
-    createdDate: '2023-31-08',
-    updatedDate: '2023-31-08',
-  },
-  {
-    id: 3,
-    name: 'Bank of America',
-    description: "Bank of America offers customers with various options",
-    bankName: "Bank of America",
-    maxCredit: 3000,
-    interestRate: 10,
-    active: true,
-    recommendedScore: '700-900',
-    annualFee: 4,
-    termsAndConditions: 'Following are the terms and conditions',
-    createdDate: '2023-31-08',
-    updatedDate: '2023-31-08',
-  },
-  {
-    id: 3,
-    name: 'Bank of America',
-    description: "Bank of America offers customers with various options",
-    bankName: "Bank of America",
-    maxCredit: 3000,
-    interestRate: 10,
-    active: true,
-    recommendedScore: '700-900',
-    annualFee: 4,
-    termsAndConditions: 'Following are the terms and conditions',
-    createdDate: '2023-31-08',
-    updatedDate: '2023-31-08',
-  },
-  {
-    id: 3,
-    name: 'Bank of America',
-    description: "Bank of America offers customers with various options",
-    bankName: "Bank of America",
-    maxCredit: 3000,
-    interestRate: 10,
-    active: true,
-    recommendedScore: '700-900',
-    annualFee: 4,
-    termsAndConditions: 'Following are the terms and conditions',
-    createdDate: '2023-31-08',
-    updatedDate: '2023-31-08',
-  },
-  {
-    id: 3,
-    name: 'Bank of America',
-    description: "Bank of America offers customers with various options",
-    bankName: "Bank of America",
-    maxCredit: 3000,
-    interestRate: 10,
-    active: true,
-    recommendedScore: '700-900',
-    annualFee: 4,
-    termsAndConditions: 'Following are the terms and conditions',
-    createdDate: '2023-31-08',
-    updatedDate: '2023-31-08',
-  },
-  {
-    id: 3,
-    name: 'Bank of America',
-    description: "Bank of America offers customers with various options",
-    bankName: "Bank of America",
-    maxCredit: 3000,
-    interestRate: 10,
-    active: true,
-    recommendedScore: '700-900',
-    annualFee: 4,
-    termsAndConditions: 'Following are the terms and conditions',
-    createdDate: '2023-31-08',
-    updatedDate: '2023-31-08',
-  }
-];
-
-@Component({
+@Component({ 
   selector: 'app-creditcards',
   templateUrl: './creditcards.component.html',
-  styleUrl: './creditcards.component.scss'
+  styleUrls: ['./creditcards.component.scss']
 })
 export class CreditcardsComponent {
 
-  displayColumns = ["select", "id", "name", "description", "bankName", "maxCredit", "interestRate", "active", "recommendedScore"]
+  creditcards: CreditCard[] = [];
 
-  dataSource = new MatTableDataSource(TABLE_DATA);
+  constructor(private creditcardsService: CreditcardsService) {
+    this.creditcardsService.getCreditCards().subscribe((data:CreditCard[]) => {
+      this.creditcards = data;
+
+      this.dataSource = new MatTableDataSource(this.creditcards);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    
+    })
+  }
+
+  dataSource = new MatTableDataSource(this.creditcards);
+
+  displayColumns = ["select", "id", "name", "description", "bankName", "maxCredit", "interestRate", "active", "recommendedScore"]
 
   selection = new SelectionModel(true, []);
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
-
-  ngAfterViewInit(){
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-  }
 
   selectHandler(row: CreditCard){
     this.selection.toggle(row as never);
